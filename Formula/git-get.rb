e40acd95b50f8a800c7f44c80d4fbb84908f8dfe
Eriks Zelenka <isindir@users.sf.net>
2020-03-28 12:32:28 +0000

    Fix

--- Formula/git-get.rb
+++ Formula/git-get.rb
@@ -1,10 +1,10 @@
 class GitGet < Formula
   desc "git-get - clone/fetch project repositories"
   homepage "https://github.com/isindir/git-get"
-  version "0.0.3"
+  version "0.0.4"
+  sha256 "3ff8664555b87b11a8417a070f6b2b90268de63239c792a090c85f5eefc77d18"
 
   url "https://github.com/isindir/git-get/archive/#{version}.tar.gz"
-  sha256 "2c95ecd63690641d749c0d58580d3f4e4c2589afa986ec5e93dfe6a5981c6c85"
   depends_on "go" => :build
 
   def install
