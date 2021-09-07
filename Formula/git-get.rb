f898874f8f599875e9bdd668818697721fb7067d
Eriks Zelenka <isindir@users.sf.net>
2021-09-07 07:33:02 +0100

    Fix typo

--- Formula/git-get.rb
+++ Formula/git-get.rb
@@ -1,9 +1,11 @@
 class GitGet < Formula
   desc "- clone/fetch project repositories"
   homepage "https://github.com/isindir/git-get"
-  url "https://github.com/isindir/git-get/archive/#{version}.tar.gz"
   version "0.0.5"
   sha256 "44972ea265f46dbdb15ee1612a4ee6d4e920df57ee5d6de6a0c76bb5892fc4cc"
+
+  url "https://github.com/isindir/git-get/archive/#{version}.tar.gz"
+
   depends_on "go" => :build
 
   def install
