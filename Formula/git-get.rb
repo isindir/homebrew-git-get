4c417e00d6fdb6bbe133da0df00bd0208d9ad536
Eriks Zelenka <isindir@users.sf.net>
2022-01-21 19:27:10 +0000

    update git-get to 0.0.9

--- Formula/git-get.rb
+++ Formula/git-get.rb
@@ -2,8 +2,8 @@ class GitGet < Formula
   desc %q{Tool to clone/fetch project repositories an-mass using Gitfile configuration file.
 git-get can also generate Gitfile from github, gitlab or bitbucket, to have all repositories user has access to.}
   homepage "https://github.com/isindir/git-get"
-  version "0.0.8"
-  sha256 "f051d26fc83d051908379ce7e49c95184a729ddd263db9e3efc5058775cba3a1"
+  version "0.0.9"
+  sha256 "02fa1dff9ca02bfb4def67cba00d1380fdb0ed0d59e01392d2317c8c3e9c41d9"
   head "https://github.com/isindir/git-get.git"
 
   url "https://github.com/isindir/git-get/archive/#{version}.tar.gz"
