eafc4856141a33a54366a410a9d85c30b56faeaa
Eriks Zelenka <isindir@users.sf.net>
2022-01-22 08:20:08 +0000

    Fix typo

--- Formula/git-get.rb
+++ Formula/git-get.rb
@@ -2,8 +2,8 @@ class GitGet < Formula
   desc %q{Tool to clone/fetch project repositories an-mass using Gitfile configuration file.
 git-get can also generate Gitfile from github, gitlab or bitbucket, to have all repositories user has access to.}
   homepage "https://github.com/isindir/git-get"
-  version "0.0.9"
-  sha256 "02fa1dff9ca02bfb4def67cba00d1380fdb0ed0d59e01392d2317c8c3e9c41d9"
+  version "0.0.10"
+  sha256 "c8775f4c4cf0cf9e6c00cb75f0895f763a11e0a2fae48c695dbabb62303c4b6a"
   head "https://github.com/isindir/git-get.git"
 
   url "https://github.com/isindir/git-get/archive/#{version}.tar.gz"
